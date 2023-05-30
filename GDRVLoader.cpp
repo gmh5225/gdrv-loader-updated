3655e2c35945425c0f377d0faba3282fe0d8a220
gmh5225 <2315157@qq.com>
2023-05-30 20:25:52 +0800

    test

--- GDRVLoader.cpp
+++ GDRVLoader.cpp
@@ -5,40 +5,45 @@ const wchar_t* DriverPath = L"C:\\Windows\\System32\\Drivers\\gdrv.sys";
 
 int wmain(int argc, wchar_t** argv)
 {
-    if (argc < 2) {
-        printf("Invalid arguments. Usage: GDRVLoader.exe TargetDriver.sys\n");
-        return false;
-    }
+
+   // printf("Usage: GDRVLoader.exe load/unload\n");
+
+ /*   char input[10];
+    printf("Load or unload driver?\n");
+    scanf("%s", input);
+	*/
+	
+    
 
     NTSTATUS Status = STATUS_UNSUCCESSFUL;
 
-    char input[10];
-    printf("Load or unload driver?\n");
-    scanf("%s", input);
 
-    if (strcmp(input, "LOAD") == 0 || strcmp(input, "load") == 0)
+    if (DropDriverFromBytes(DriverPath))
     {
-        if (DropDriverFromBytes(DriverPath))
-        {
-            // Load driver
-            Status = WindLoadDriver((PWCHAR)DriverPath, argv[1], FALSE);
+        // Load driver
+        Status = WindLoadDriver((PWCHAR)DriverPath, argv[1], FALSE);
 
-            if (NT_SUCCESS(Status))
-                printf("Driver loaded successfully\n");
-
-            DeleteFile((PWSTR)DriverPath);
-        }
-    }
-    else if (strcmp(input, "Unload") == 0 || strcmp(input, "unload") == 0)
-    {
-        // Unload driver
-        Status = WindUnloadDriver((PWCHAR)argv[1], 0);
         if (NT_SUCCESS(Status))
-            printf("Driver unloaded successfully\n");
+            printf("Driver loaded successfully\n");
+
+        DeleteFile((PWSTR)DriverPath);
     }
+	
+    /*if (strcmp(input, "LOAD") == 0 || strcmp(input, "load") == 0)
+    {
+       
+    }*/
+    //else if (strcmp(input, "Unload") == 0 || strcmp(input, "unload") == 0)
+    //{
+    //    // Unload driver
+    //    Status = WindUnloadDriver((PWCHAR)argv[1], 0);
+    //    if (NT_SUCCESS(Status))
+    //        printf("Driver unloaded successfully\n");
+    //}
 
     if (!NT_SUCCESS(Status))
         printf("Error: %08X\n", Status);
-
+    else
+        printf("OKOK: %08X\n", Status);
     return true;
 }
